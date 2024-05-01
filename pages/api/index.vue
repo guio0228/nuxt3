0c932c2a427fe0409a986f4ed319587f42f3b64d
guio0228 <93069491+guio0228@users.noreply.github.com>
2024-05-02 02:19:53 +0800

    測試ts

--- pages/api/index.vue
+++ pages/api/index.vue
@@ -3,16 +3,36 @@
 //   let arr1:(string | number)[]=['aaa',131];
 // console.log(arr);
 
-interface Person{
-  name:string;
-  age:number;
-  sex?:string;
-}
-let putico:Person={
-  name:'putico',
-  age:18,
-}
-console.log(putico);
+// interface Person{
+//   name:string;
+//   age:number;
+//   sex?:string;
+//   [propName:string]:any;
+// }
+// let putico:Person={
+//   name:'putico',
+//   age:18,
+// }
+// console.log(putico);
+////////////////////////////////
+// function Add(x: number, y: number): number;
+// function Add(x: string, y: string): string;
+// function Add(x: number | string, y: number | string): number | string {
+//     if (typeof x === 'number' && typeof y === 'number') {
+//         return x + y;
+//     } else if (typeof x === 'string' && typeof y === 'string') {
+//         return x + y;
+//     } else {
+//         throw new Error('Invalid argument types for Add function');
+//     }
+// }
+// console.log(Add(1,2));
+// console.log(Add('1','2'));
+type stringType = '咪' | '喵' | '貓';
+
+let str:stringType='咪';
+
+
 </script>
 
 <template>
